9c826e61bc3a577df8dc2fbebb6f165aa6bb0ff3
unknown <brianguta2000@yahoo.com>
2019-08-22 21:08:02 -0400

    todo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <ToDo />
-        <ToDo />
-      </ul>
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk that cat again', isComplete: true },
+        { description: 'Throw the dishes away instead of wash them', isComplete: false },
+        { description: 'Buy new dishes cause I am getting hungry again', isComplete: true }
+      ]
+    };
+  }
+
+   render() {
+     return (
+       <div className="App">
+        <ul>
+            { this.state.todos.map( (todo, index) =>
+              <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            )}
+        </ul>
+       </div>
   );
+ }
 }
 
 export default App;
